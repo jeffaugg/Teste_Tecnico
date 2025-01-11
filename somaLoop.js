00a9beef9b09000bfa52bdacdf01f53690844d86
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-11 15:25:17 -0300

    1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0; Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } Imprimir(SOMA); Ao final do processamento, qual será o valor da variável SOMA?

---
+++ somaLoop.js
@@ -0,0 +1,14 @@
+
+// Definição das variáveis
+const INDICE = 13;
+let soma = 0;
+let k = 0;
+
+// Loop 'while' que soma os números de 1 até INDICE
+while (k < INDICE) {
+    k += 1;
+    soma += k;
+}
+
+// Impressão do resultado
+console.log(`O valor da variável SOMA ao final do processamento é: ${soma}`); //91
